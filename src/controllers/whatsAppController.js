b4397884c52b0d3e63d45c10a28bdc37d52ac674
Facundo <facundofigueroa789@gmail.com>
2024-05-27 11:27:05 -0300

    20. Recibir mensajes desde WhatsApp

--- src/controllers/whatsAppController.js
+++ src/controllers/whatsAppController.js
@@ -1,3 +1,6 @@
+// import fs from 'node:fs';
+// const myConsole = new console.Console(fs.createWriteStream('./logs.txt'))
+
 export const VerifyToken = (req, res)=>{
   try {
     let accessToken = 'KJALSDHBAJKL1769623EBKJAS971'; 
@@ -15,5 +18,14 @@ export const VerifyToken = (req, res)=>{
 
 }
 export const RecivedMessage = (req, res)=>{
-  res.send('askh,d');
+  try {
+    const entry = req.body.entry[0]
+    const changes = entry.changes[0]
+    const {value: {messages} } = changes;
+    console.log({messages});
+    // myConsole.log(messages)
+    res.send('EVENT_RECIBED')
+  } catch (error) {
+    res.send('EVENT_RECIBED')
+  }
 }
