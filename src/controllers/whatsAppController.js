93f194c501d2e3014557f73a460ab7592f2c8bf7
Facundo <facundofigueroa789@gmail.com>
2024-05-27 10:57:36 -0300

    19. Configurar Webhook WhatsApp para verificación de token

--- src/controllers/whatsAppController.js
+++ src/controllers/whatsAppController.js
@@ -1,6 +1,19 @@
 export const VerifyToken = (req, res)=>{
-  res.send('askh,d')
+  try {
+    let accessToken = 'KJALSDHBAJKL1769623EBKJAS971'; 
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+    if (challenge != null && accessToken != null && accessToken === token) {
+      res.send(challenge);
+    } else {
+    res.send(400).send(error);
+      
+    }
+  } catch (error) {
+    res.send(400).send(error);
+  }
+
 }
 export const RecivedMessage = (req, res)=>{
-  res.send('askh,d')
+  res.send('askh,d');
 }
