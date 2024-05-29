c3938b6d22f9cb7ee38a1b00affcb2910ce99f6a
Facundo <facundofigueroa789@gmail.com>
2024-05-29 13:59:19 -0300

    saDSA

--- src/services/whatsApp.service.js
+++ src/services/whatsApp.service.js
@@ -1,18 +1,28 @@
+// import https from "node:https"
 
-export const sendMessageWhatsApp =  async (textResponse, number)=>{
-  console.log({textResponse, number});
+export const sendMessageWhatsApp = async  (textResponse, number)=>{
   const cleanedNumber = number.slice(0, 2) + number.slice(3)
   console.log(cleanedNumber);
   const body = {
-    messaging_product: "whatsapp",
-    to: number,
-    text: {
-      body: textResponse
+    "messaging_product": "whatsapp",
+    "to": number,
+    "text": {
+      "body": textResponse
     },
-    type: "text"
+    "type": "text"
   };
+  console.log({body});
   const data = JSON.stringify(body);
- 
+  // const options = {
+  //   host: "graph.facebook.com",
+  //   path: "/v19.0/347629235091879/messages",
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: "Bearer EAAGprZCTZAHtABO6A3GQa1rbHqe75B1ILlDtPFDZAminyA23KKrbwYZA8IDR3lTOS8KVkrMJmr6EI19pzIbYVZB9iStswT0SR3dqrSX9meaDq9ZAIAupynaINBK4pDYN3ruoWzTHgYPmtXCHQK6aZCENmnzxWaOIy4Eil9YlrwoyeFVXnYQV1B4btDMzwNqTOh3SuZBHvHtIrdQXNZCZAJvrAYP90AZCptqsihANkeyUdwZD",
+  //   },
+  // };
+
   const dataRes = await fetch('https://graph.facebook.com/v19.0/347629235091879/messages', {
     method: "POST",
     headers: {
