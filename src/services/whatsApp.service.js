66c440fcc14b22a625bffe271893f3411d01759b
Facundo <facundofigueroa789@gmail.com>
2024-05-29 13:48:39 -0300

    sdaas

--- src/services/whatsApp.service.js
+++ src/services/whatsApp.service.js
@@ -1,6 +1,5 @@
-import https from "node:https"
 
-export const sendMessageWhatsApp =  (textResponse, number)=>{
+export const sendMessageWhatsApp =  async (textResponse, number)=>{
   console.log({textResponse, number});
   const cleanedNumber = number.slice(0, 2) + number.slice(3)
   console.log(cleanedNumber);
@@ -13,39 +12,16 @@ export const sendMessageWhatsApp =  (textResponse, number)=>{
     type: "text"
   };
   const data = JSON.stringify(body);
-  const options = {
-    host: "graph.facebook.com",
-    path: "/v19.0/347629235091879/messages",
+ 
+  const dataRes = await fetch('https://graph.facebook.com/v19.0/347629235091879/messages', {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Bearer EAAGprZCTZAHtABO6A3GQa1rbHqe75B1ILlDtPFDZAminyA23KKrbwYZA8IDR3lTOS8KVkrMJmr6EI19pzIbYVZB9iStswT0SR3dqrSX9meaDq9ZAIAupynaINBK4pDYN3ruoWzTHgYPmtXCHQK6aZCENmnzxWaOIy4Eil9YlrwoyeFVXnYQV1B4btDMzwNqTOh3SuZBHvHtIrdQXNZCZAJvrAYP90AZCptqsihANkeyUdwZD",
+      "Authorization": "Bearer EAAGprZCTZAHtABO6A3GQa1rbHqe75B1ILlDtPFDZAminyA23KKrbwYZA8IDR3lTOS8KVkrMJmr6EI19pzIbYVZB9iStswT0SR3dqrSX9meaDq9ZAIAupynaINBK4pDYN3ruoWzTHgYPmtXCHQK6aZCENmnzxWaOIy4Eil9YlrwoyeFVXnYQV1B4btDMzwNqTOh3SuZBHvHtIrdQXNZCZAJvrAYP90AZCptqsihANkeyUdwZD",
     },
-  };
-  console.log('---------------------------------------- ANTES DEL REQ ----------------------------------------');
-  const req = https.request(options, (res) => {
-    console.log('---------------------------------------- DENTRO DEL REQ ----------------------------------------');
-    let responseData = '';
-    
-    res.on('data', (chunk) => {
-      responseData += chunk;
-    });
-    
-    res.on('end', () => {
-      console.log('Response from WhatsApp API: ', responseData);
-      const responseJson = JSON.parse(responseData);
-      if (res.statusCode === 200) {
-        console.log('Message sent successfully:', responseJson);
-      } else {
-        console.error('Error sending message:', responseJson);
-      }
-    });
-  });
-  console.log('---------------------------------------- DESPUÉS DEL REQ ----------------------------------------');
-  
-  req.on('error', (e) => {
-    console.error('Error en el request => ', e);
-  });
-  req.write(data);
-  req.end();
+    body: data
+  })
+  const respuesta = await dataRes.json()
+  console.log("dataRes => ", dataRes);
+  console.log("respuesta => ", respuesta);
 }
