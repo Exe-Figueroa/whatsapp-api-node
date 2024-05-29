ed9a46a78f3bbe0173d03a53bdc78c70fbc48908
Facundo <facundofigueroa789@gmail.com>
2024-05-29 12:53:48 -0300

    coso

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 3000;
 app.use(json())
 
 app.use('/whatsapp',WhatsAppRouter)
-
+app.get('/', (req, res)=>{
+  res.send(`corriendo en vercel en el puerto ${port}`)
+})
 app.listen(port, ()=>{
   console.log(`escuchando en el puerto ${port}`);
 })
